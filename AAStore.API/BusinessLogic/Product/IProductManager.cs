d2c26ba0dfab48e612ca35415947d7cdf7a1489b
asp4391 <121432998+asp4391@users.noreply.github.com>
2023-01-03 18:17:40 +0530

    Completed Product API

--- AAStore.API/BusinessLogic/Product/IProductManager.cs
+++ AAStore.API/BusinessLogic/Product/IProductManager.cs
@@ -6,6 +6,13 @@ namespace AAStore.API.BusinessLogic.Product
     public interface IProductManager
     {
         List<ProductModel> GetProducts();
+        ProductModel GetProductByProductId(int id);
+        List<ProductModel> GetProductsByCategory(int id);
+        List<ProductModel> GetProductsByCompany(int id);
+        public bool AddProduct(ProductModel product);
+        public bool UpdateProduct(int id,ProductModel product);
+        public bool DeleteProduct(int id );
+        
         
     }
 }
--- AAStore.API/Repository/Product/ProductRepository.cs
+++ AAStore.API/Repository/Product/ProductRepository.cs
@@ -20,7 +20,7 @@ namespace AAStore.API.Repository.Product
         {
             var connectionString = _configuration.GetConnectionString("Product");
             var ds = new DataSet();
-             using (SqlConnection con = new SqlConnection(connectionString))
+            using (SqlConnection con = new SqlConnection(connectionString))
             {
                 SqlCommand cmd = new SqlCommand(Procedures.GET_PRODUCT_DETAILS ,con);
                 cmd.CommandType = CommandType.StoredProcedure;
@@ -34,12 +34,132 @@ namespace AAStore.API.Repository.Product
             return ds;
         }
 
+        public DataSet GetProductsById(int id)
+        {
+            var connectionString = _configuration.GetConnectionString("Product");
+            var ds = new DataSet();
+
+            using(SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(Procedures.GET_PRODUCT_DETAILS_BY_PRODUCT,con);
+                cmd.Parameters.AddWithValue(CreateParameters.PRODUCT_ID,id);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = 0;
+
+                var sqlDataAdapter = new SqlDataAdapter(cmd);
+                sqlDataAdapter.Fill(ds);
+            }
+            return ds;
+
+        }
+
+        public DataSet GetProductsByCategory(int id)
+        {
+            var connectionString = _configuration.GetConnectionString("Product");
+            var ds = new DataSet();
+
+            using(SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(Procedures.GET_PRODUCT_DETAILS_BY_CATEGORY,con);
+                cmd.Parameters.AddWithValue(CreateParameters.CATEGORY_ID,id);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = 0;
+
+                var sqlDataAdapter = new SqlDataAdapter(cmd);
+                sqlDataAdapter.Fill(ds);
+            }
+            return ds;
+
+        }
+
+        public DataSet GetProductsByCompany(int id)
+        {
+            var connectionString = _configuration.GetConnectionString("Product");
+            var ds = new DataSet();
+
+            using(SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(Procedures.GET_PRODUCT_DETAILS_BY_COMPANY,con);
+                cmd.Parameters.AddWithValue(CreateParameters.COMPANY_ID,id);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = 0;
+
+                var sqlDataAdapter = new SqlDataAdapter(cmd);
+                sqlDataAdapter.Fill(ds);
+            }
+            return ds;
+        }
+
+        public DataSet AddProduct(ProductModel product)
+        {
+            var connectionString = _configuration.GetConnectionString("Product");
+            var ds = new DataSet();
+
+            using(SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(Procedures.ADD_PRODUCT,con);
+                cmd.Parameters.AddWithValue(CreateParameters.PRODUCT_NAME,product.ProductName);
+                cmd.Parameters.AddWithValue(CreateParameters.PRICE,product.Price);
+                cmd.Parameters.AddWithValue(CreateParameters.PIMAGE,product.PImage);
+                cmd.Parameters.AddWithValue(CreateParameters.CATEGORY_ID,product.CategoryId);
+                cmd.Parameters.AddWithValue(CreateParameters.COMPANY_ID,product.CompanyId);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = 0;
+
+                var sqlDataAdapter = new SqlDataAdapter(cmd);
+                sqlDataAdapter.Fill(ds);
+            }
+            return ds;
+        }
+
+        public DataSet UpdateProduct(int id,ProductModel product)
+        {
+            var connectionString = _configuration.GetConnectionString("Product");
+            var ds = new DataSet();
+
+            using(SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(Procedures.UPDATE_PRODUCT_DETAILS,con);
+                cmd.Parameters.AddWithValue(CreateParameters.PRODUCT_ID,id);
+                cmd.Parameters.AddWithValue(CreateParameters.PRODUCT_NAME,product.ProductName);
+                cmd.Parameters.AddWithValue(CreateParameters.PRICE,product.Price);
+                cmd.Parameters.AddWithValue(CreateParameters.PIMAGE,product.PImage);
+                cmd.Parameters.AddWithValue(CreateParameters.CATEGORY_ID,product.CategoryId);
+                cmd.Parameters.AddWithValue(CreateParameters.COMPANY_ID,product.CompanyId);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = 0;
+
+                var sqlDataAdapter = new SqlDataAdapter(cmd);
+                sqlDataAdapter.Fill(ds);
+            }
+            return ds;
+        }
+
+        public DataSet DeleteProduct(int id)
+        {
+            var connectionString = _configuration.GetConnectionString("Product");
+            var ds = new DataSet();
+
+            using(SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(Procedures.DELETE_PRODUCT,con);
+                cmd.Parameters.AddWithValue(CreateParameters.PRODUCT_ID,id);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = 0;
+
+                var sqlDataAdapter = new SqlDataAdapter(cmd);
+                sqlDataAdapter.Fill(ds);
+            }
+            return ds;
+        }
+
         public static class CreateParameters { 
             public const string PRODUCT_NAME = "@ProductName";
             public const string PRICE = "@Price";
             public const string PIMAGE = "@PImage";
             public const string CATEGORY_ID = "@CategoryId";
             public const string COMPANY_ID = "@CompanyId";
+            public const string PRODUCT_ID = "@ProductId";
         }
 
         public static class Procedures
